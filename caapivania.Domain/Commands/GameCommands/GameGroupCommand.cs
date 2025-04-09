7b73295016f3ebef31a1b91cb9d0d42a9c8e55cc
lucasApalosqui <lucas.apalosqui@gmail.com>
2025-04-09 16:04:40 -0300

    :sparkles: feat: add GameGroupCommand

---
+++ caapivania.Domain/Commands/GameCommands/GameGroupCommand.cs
@@ -0,0 +1,29 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace caapivania.Domain.Commands.GameCommands
+{
+    public class GameGroupCommand : Notifiable<Notification>
+    {
+        public GameGroupCommand() { }
+
+        public GameGroupCommand(Guid gameId, Guid groupId)
+        {
+            GameId = gameId;
+            GroupId = groupId;
+        }
+
+        public Guid GameId { get; set; }
+        public Guid GroupId { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(
+                new Contract<GameGroupCommand>()
+                    .Requires()
+                    .IsNotNull(GameId, "Game", "Game must be not null")
+                    .IsNotNull(GroupId, "Group", "Group must be not null")
+             );
+        }
+    }
+}
